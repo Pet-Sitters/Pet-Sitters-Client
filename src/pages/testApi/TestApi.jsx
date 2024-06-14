8bf804d73237a4be1cd4c4c3eb026919e43f52f9
Kiryl Dorakh <kirill.dorokh@gmail.com>
2024-06-14 21:10:37 +0400

    api test

--- src/pages/testApi/TestApi.jsx
+++ src/pages/testApi/TestApi.jsx
@@ -2,13 +2,39 @@ import React from 'react';
 import FormButton from "../../components/UI/Buttons/FormButton/FormButton.jsx";
 import {Form} from "antd";
 
+import PetService from "./api/services/PetService.js"
+
 import s from './TestApi.module.scss'
 
-const TestApi = () => {
-    const [form] = Form.useForm()
 
-    const handleFinish = (values) => {
+const TestApi = () => {
+    const [form1] = Form.useForm()
+    const [form2] = Form.useForm()
+
+    const handleFinish1 = async (values) => {
         console.log('Form values:', values)
+        console.log(localStorage.getItem('accessToken'))
+
+        try {
+            const response = await PetService.getUser()
+            console.log(response)
+        } catch (e){
+            console.log(e)
+        }
+
+    }
+
+    const handleFinish2 = async (values) => {
+        console.log('Form values:', values)
+        console.log(localStorage.getItem('accessToken'))
+
+        try {
+            const response = await PetService.fetchPets()
+            console.log(response)
+        } catch (e){
+            console.log(e)
+        }
+
     }
 
     const onFinishFailed = (errorInfo) => {
@@ -17,20 +43,38 @@ const TestApi = () => {
 
     return (
         <div className={s.test}>
-            <Form
-                form={form}
-                layout='vertical'
-                onFinishFailed={onFinishFailed}
-                onFinish={handleFinish}
-            >
-                <FormButton
-                    type='primary'
-                    htmlType='submit'
+            <div>
+                <Form
+                    form={form1}
+                    layout='vertical'
+                    onFinishFailed={onFinishFailed}
+                    onFinish={handleFinish1}
                 >
-                    test request
-                </FormButton>
-            </Form>
+                    <FormButton
+                        type='primary'
+                        htmlType='submit'
+                    >
+                        test user request
+                    </FormButton>
+                </Form>
+            </div>
+            <div>
+                <Form
+                    form={form2}
+                    layout='vertical'
+                    onFinishFailed={onFinishFailed}
+                    onFinish={handleFinish2}
+                >
+                    <FormButton
+                        type='primary'
+                        htmlType='submit'
+                    >
+                        test pets request
+                    </FormButton>
+                </Form>
+            </div>
         </div>
+
     );
 };
 
--- src/pages/testApi/api/http/index.js
+++ src/pages/testApi/api/http/index.js
@@ -1,15 +1,16 @@
 import axios from "axios";
 
-const BASE_URL = "https://gateway.scan-interfax.ru/api/v1";
+const BASE_URL = "http://89.223.123.5/";
 
 const $api = axios.create({
     withCredentials: false,
     baseURL: BASE_URL
 })
 
-$api.interceptors.request.use((config) => {
-    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
-    return config;
-})
+
+// $api.interceptors.request.use((config) => {
+//     config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+//     return config;
+// })
 
 export default $api;

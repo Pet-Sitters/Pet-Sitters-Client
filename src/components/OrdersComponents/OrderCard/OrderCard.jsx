adad5ba8249e9cee2ed2ab4a1ae0ce9dc0b5a506
Kiryl Dorakh <kirill.dorokh@gmail.com>
2024-06-21 19:40:11 +0400

    order card styles

--- src/components/OrdersComponents/OrderCard/OrderCard.jsx
+++ src/components/OrdersComponents/OrderCard/OrderCard.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import s from './OrderCard.module.scss';
 
+import clockImg from '../../../../public/assets/images/account/status/clock-Light.svg'
+import confirmImg from '../../../../public/assets/images/account/status/check-circle-Light.svg'
+
 const OrderCard = () => {
     return (
         <div className={s.order}>
-            <div>
-                <p>Передержка вид животного + Кличка</p>
-                <p>Даты: </p>
-                <p>Ситтер: </p>
-                <p>Cмотреть детали заказа</p>
+            <div className={s.infoContainer}>
+                <h1 className={s.title}>Передержка вид животного + Кличка</h1>
+                <p className={s.info}>Даты: </p>
+                <p className={s.info}>Ситтер: </p>
+                <p className={s.details}>Cмотреть детали заказа</p>
             </div>
-            <div>
-                <p className={s.status}>В ожидании
-                    подтверждения</p>
+            <div className={s.status}>
+                <div className={s.statusImg}>
+                    <img src={clockImg} alt='process'/>
+                </div>
+                <h1 className={s.statusText}>В ожидании подтверждения</h1>
             </div>
 
         </div>
